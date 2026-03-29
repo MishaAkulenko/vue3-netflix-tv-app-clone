ffd5f20bec06754920948ab3a5f4d766ce0d8f32
MishaAkulenko <mishaakulenko@gmail.com>
2026-03-29 19:36:21 +0300

    Change default theme from light to dark in global constants

--- src/constants/globalConst.ts
+++ src/constants/globalConst.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_LOCALE = 'en';
 export const FALLBACK_LOCALE = 'en';
 export const LOCALES = ['uk', 'en'];
-export const DEFAULT_THEME = 'light';
+export const DEFAULT_THEME = 'dark';
 export const THEMES = ['light', 'dark'] as const;
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const APP_HEADER_ID = `AppHeader`;
